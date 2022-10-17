56ad3239f2207a0a58a399f5bc083b79aecb159c
Haoyu Guo <guohaoyuhk@163.com>
2022-10-18 00:00:51 +0200

    creat project

--- main.py
+++ main.py
@@ -5,8 +5,16 @@ import numpy as np
 import pandas as pd
 
 def read_data():
-    fm = pd.read_excel(r"D:\Desktop\data visualization-dataset\Mental health Depression disorder Data.xlsx", sheet_name="Sheet1")  # 用该方法读取表格和表单里的单元格的数据
-    print(fm)
+    sheet1 = pd.read_excel(r"Mental health Depression disorder Data.xlsx", sheet_name="prevalence-by-mental-and-substa")  # 用该方法读取表格和表单里的单元格的数据
+    sheet2 =pd.read_excel(r"Mental health Depression disorder Data.xlsx", sheet_name="depression-by-level-of-educatio")
+    sheet3 = pd.read_excel(r"Mental health Depression disorder Data.xlsx", sheet_name="prevalence-of-depression-by-age")
+    sheet4 = pd.read_excel(r"Mental health Depression disorder Data.xlsx", sheet_name="prevalence-of-depression-males-")
+    sheet5 = pd.read_excel(r"Mental health Depression disorder Data.xlsx", sheet_name="suicide-rates-vs-prevalence-of-")
+    print(sheet1)
+    print(sheet2)
+    print(sheet3)
+    print(sheet4)
+    print(sheet5)
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     read_data()
