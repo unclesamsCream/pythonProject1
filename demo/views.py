e088719ab3e23b48ac5d15f42ac84eaa177822a1
Haoyu Guo <guohaoyuhk@163.com>
2022-10-20 11:34:50 +0200

    draw graph

--- demo/views.py
+++ demo/views.py
@@ -1,10 +1,15 @@
-import json
-import math
-import random
-import numpy as np
+from jinja2 import Environment, FileSystemLoader
+from pyecharts.globals import CurrentConfig
+from django.http import HttpResponse
 import pandas as pd
+# CurrentConfig.GLOBAL_ENV = Environment(loader=FileSystemLoader("./demo/templates"))
+
+from pyecharts import options as opts
 from pyecharts.charts import Bar
-def read_data():
+
+
+def index(request):
+    # print(request)
     sheet1 = pd.read_excel(r"Mental health Depression disorder Data.xlsx", sheet_name="prevalence-by-mental-and-substa")  # 用该方法读取表格和表单里的单元格的数据
     sheet2 =pd.read_excel(r"Mental health Depression disorder Data.xlsx", sheet_name="depression-by-level-of-educatio")
     sheet3 = pd.read_excel(r"Mental health Depression disorder Data.xlsx", sheet_name="prevalence-of-depression-by-age")
@@ -15,21 +20,12 @@ def read_data():
     x = years_count.index.tolist()
     x = [int(num) for num in x]
     y = years_count.values.tolist()
-    # y = years_list.loc[years_list.index].values
-    print(type(x[0]))
-    print(type(y[0]))
-    bar = Bar()
-    bar.add_xaxis(x)
-    bar.add_yaxis("year",y)
-    bar.render()
-
-    # print(sheet1)
-    # print(sheet2)
-    # print(sheet3)
-    # print(sheet4)
-    # print(sheet5)
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    read_data()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    c = (
+        Bar()
+        .add_xaxis(x)
+        .add_yaxis("year",y)
+        .set_global_opts(title_opts=opts.TitleOpts(title="Bar-基本示例", subtitle="我是副标题"))
+    )
+    # print(c)
+    c.render()
+    return HttpResponse(c.render_embed())
